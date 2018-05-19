256fc8feee558d245a6df905f8c4050134de911d
zoizer <zoizer123@hotmail.com>
2018-05-19 12:12:52 +0200

    Added Product container + product

--- imat/src/imat/fxml/container/FXMLProductContainer.java
+++ imat/src/imat/fxml/container/FXMLProductContainer.java
@@ -5,9 +5,15 @@
  */
 package imat.fxml.container;
 
+import imat.fxml.item.FXMLProductItem;
 import java.io.IOException;
+import java.util.List;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
 
 /**
  * FXML extension class
@@ -15,6 +21,9 @@ import javafx.scene.layout.AnchorPane;
  * @author Zoizer
  */
 public class FXMLProductContainer extends AnchorPane {
+    @FXML
+    private VBox vb;
+    
     public FXMLProductContainer() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/imat/fxml/container/FXMLProductContainer.fxml"));
         fxmlLoader.setRoot(this);
@@ -24,6 +33,10 @@ public class FXMLProductContainer extends AnchorPane {
             fxmlLoader.load();
         } catch (IOException exception) {
             throw new RuntimeException(exception);
+        }
+        List<Product> p = IMatDataHandler.getInstance().getProducts();
+        for (Product e : p) {
+            vb.getChildren().add(new FXMLProductItem(e));
         }
     }
 }
