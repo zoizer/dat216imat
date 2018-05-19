9a62bc63b912974b126a0abfb79348a8160789bd
Max <blizzlord@gmail.com>
2018-05-19 20:33:11 +0200

    Set a default window size of 1100px by 800px.

--- imat/src/imat/Imat.java
+++ imat/src/imat/Imat.java
@@ -21,7 +21,7 @@ public class Imat extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/FXMLApplication.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 1100, 800);
         
         stage.setScene(scene);
         stage.show();
