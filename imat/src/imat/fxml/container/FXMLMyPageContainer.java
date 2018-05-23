a269d16564051792531bf945da5520c8146f111e
Joel Sundqvist <joelsundqvist@live.se>
2018-05-23 09:44:10 +0200

    adding comboboxes and writing to creditcard

--- imat/src/imat/fxml/container/FXMLMyPageContainer.java
+++ imat/src/imat/fxml/container/FXMLMyPageContainer.java
@@ -9,10 +9,12 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import se.chalmers.cse.dat216.project.CreditCard;
 import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
@@ -29,12 +31,16 @@ public class FXMLMyPageContainer extends AnchorPane {
 
     //myPage
     @FXML
-    private TextField nameText, streetText, postcodeText, cityText, phoneText ;
+    private TextField nameText, streetText, postcodeText, cityText, phoneText, CardNumberText, CVCText;
+    @FXML
+    private ComboBox cardTypeText, ValidMonthText, ValidYearText;
 
 
     public FXMLMyPageContainer() {
         IMatDataHandler handler = IMatDataHandler.getInstance();
         Customer customer = handler.getCustomer();
+        CreditCard creditcard = handler.getCreditCard();
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/imat/fxml/container/FXMLMyPageContainer.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -44,6 +50,54 @@ public class FXMLMyPageContainer extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+        //For cardtype
+        cardTypeText.getItems().addAll("Visa", "MasterCard", "American Express", "Diners Club");
+        cardTypeText.getSelectionModel().select("Visa");
+        cardTypeText.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                creditcard.setCardType(newValue);
+            }
+        });
+        //For Valid Month
+        ValidMonthText.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        ValidMonthText.getSelectionModel().select(0);
+        ValidMonthText.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+                creditcard.setValidMonth(newValue);
+            }
+        });
+        //For Valid Year
+        ValidYearText.getItems().addAll(2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025);
+        ValidYearText.getSelectionModel().select(0);
+        ValidYearText.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+                creditcard.setValidYear(newValue);
+            }
+        });
+        // Cardnumber
+        CardNumberText.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable,
+                                String oldValue, String newValue) {
+
+                creditcard.setCardNumber(newValue);
+            }
+        });
+        //cvc
+        CVCText.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable,
+                                String oldValue, String newValue) {
+
+                creditcard.setVerificationCode(Integer.parseInt(newValue));
+            }
+        });
 
         // For name
         nameText.textProperty().addListener(new ChangeListener<String>() {
